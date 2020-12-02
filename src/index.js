9eeb9384b321afe1d562d4b4dafa2a13ecdc50a4
mizen <45590393+mizunoyuuichi@users.noreply.github.com>
2020-12-02 15:55:40 +0900

    adding spread syntax

--- src/index.js
+++ src/index.js
@@ -98,5 +98,10 @@
 // sayHello()
 
 // スプレット構文 ...
-const arr1 = [1, 2];
-console.log(arr1);
+const arr1 = [3, 2, 1];
+// console.log(arr1);
+// console.log(...arr1);
+
+const sumFunc = (num1, num2) => console.log(num1 + num2);
+// sumFunc(arr1[0], arr1[1])
+sumFunc(...arr1); // same up
